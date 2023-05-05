111e990447a38826f049eacd125f4495747aa53b
RohGs <ronaldogoudinho815@gmail.com>
2023-05-05 00:22:23 -0300

    css improvement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { JobsComponent } from './views/jobs/jobs.component';
 import { CompaniesComponent } from './views/companies/companies.component';
-import { CompanieComponent } from './components/companie/companie.component';
-import { JobComponent } from './components/job/job.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ListCompaniesComponent } from './components/list-companies/list-companies.component';
@@ -22,8 +20,6 @@ import { AddCompanieComponent } from './components/add-companie/add-companie.com
     AppComponent,
     JobsComponent,
     CompaniesComponent,
-    CompanieComponent,
-    JobComponent,
     HeaderComponent,
     ListCompaniesComponent,
     AddCompanieComponent,
